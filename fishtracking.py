ca12e3bb6314d7de9f5cf9bc955378a3cd848201
byfinger <wjddnjsdnd@gmail.com>
2021-09-27 21:29:20 +0900

    aa

--- fishtracking.py
+++ fishtracking.py
@@ -1,5 +1,6 @@
 import cv2
 import socketio
+import numpy as py
 import pybase64
 
 sv = True
@@ -13,23 +14,23 @@ def stopvideo(data):
 cap = cv2.VideoCapture(1)
 cap.set(3, 960)
 cap.set(4, 540)
-
+posxcheck = []
+posycheck = []
 mog = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
 while True:
     if sv:
         posx = []
         posy = []
-        posxcheck = []
-        posycheck = []
-        sendposx = []
-        sendposy = []
         i=0
+        j=0
+        sum = 0
+        v = 0
         ret, frame = cap.read()
         mogframe = mog.apply(frame)
         contours, _ = cv2.findContours(mogframe, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for c in contours:
             mmt = cv2.moments(c)
-            if mmt['m00'] > 20:
+            if mmt['m00'] > 70:
                 i=i+1
                 x, y, w, h = cv2.boundingRect(c)
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 3)
@@ -37,25 +38,24 @@ while True:
                 cy = int(mmt['m01']/mmt['m00'])
                 posx.insert(i, cx)
                 posy.insert(i, cy)
-
-        
-            #pos = '(' + str(cx) + ',' + str(cy) + ')'
-            #cv2.putText(frame, pos, (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (157, 255, 0), 1, cv2.LINE_AA)
-        if len(posx) != len(posxcheck):
-            if len(posx) > len(posxcheck):
-                for a in len:
-                    
-
-
-        sio.emit('posdata', {'x': posx, 'y': posy}, namespace='/video')
+                #pos = str(mmt['m00'])
+                #cv2.putText(frame, pos, (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (157, 255, 0), 1, cv2.LINE_AA)
+        if len(posx) == len(posxcheck) and len(posx) != 0:
+            for a in posx:
+                sum = sum + ((posx[j] - posxcheck[j])**2 + (posy[j] - posycheck[j])**2)**0.5
+                j += 1
+            v = sum/len(posx)
+        else:
+            v = -1
+                
+        sio.emit('posdata', {'x': posx, 'y': posy, 'v': v }, namespace='/video')
         #cv2.imshow('video view', mogframe)
-        posxcheck = posx
-        posycheck = posy
         res, sframe = cv2.imencode('.jpg', frame)
         data = pybase64.b64encode(sframe)
         sio.emit('videocamera', data, namespace='/video')
         cv2.imshow('recttangle view', frame)
-    
+        posxcheck = posx
+        posycheck = posy
         k = cv2.waitKey(1) & 0xFF
         if k == 27:
             break
--- test.py
+++ test.py
@@ -1,21 +1,24 @@
 posx = [10,20,22,26,50]
-posx1 = [21,22,23]
+posx1 = [11,22,23,22,45]
 posxf = []
 mind = [0]
 i=0
-if len(posx) != len(posx1):
-  if len(posx) > len(posx1):
-    mind = posx
+if len(posx) == len(posx1):
     for a in posx:
-        t=100
-        for b in posx1:
-            if abs(a-b) < t and abs(a-b) < 4:
-             del mind[i]
-             mind.insert(i, b)
-             t=abs(a-b)
-            else:
-             del mind[i]
-             mind.insert(i, a)
-             t=abs(a-b)
-        i += 1
-    print(mind)
+      
+      posxf.append(abs(posx[i]-posx1[i]))
+      print(i)
+      i += 1
+        #t=100
+        ##for b in posx1:
+        ##    if abs(a-b) < t and abs(a-b) < 4:
+        ##     del mind[i]
+        ##     mind.insert(i, b)
+        #     t=abs(a-b)
+        #    else:
+        #     del mind[i]
+        #     mind.insert(i, a)
+        #     t=abs(a-b)
+        # i += 1
+        #print(mind)
+    print(posxf)
